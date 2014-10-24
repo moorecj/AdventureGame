402521911b0f9718ae72ffae08b7d763934acb6d
Cyril Moore <cmoore@foxguardsolutions.com>
2014-10-24 15:22:54 -0400

    Initial commit of state machine adventure kata. Basic tests pass. Added a git ignore file.

---
+++ AdventureKata/AdventureStateMachine.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureKata
+{
+    public class AdventureStateMachine
+    {
+        private string currentState;
+
+        public AdventureStateMachine()
+        {
+            currentState   = "             Atomic Coffee Proudly Presents THE ADVENTURE KATA ";
+            currentState  += "\n                    : created and written by Princie :         ";
+            currentState  += "\n                              with help from           ";
+            currentState  += "\n             http://www.lincolnli.com/text-adventure-game---c.html         ";
+        }
+
+
+        public void Input(string p)
+        {
+            
+        }
+
+        public string GetState()
+        {
+            return currentState;
+        }
+    }
+}
---
+++ AdventureStateMachineTests/StateMachineTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using AdventureKata;
+using Moq;
+
+namespace AdventureStateMachineTests
+{
+    [TestFixture]
+    public class StateMachineTests
+    {
+        [Test]
+        public void AStateMachineShouldNotBeNull()
+        {
+            AdventureStateMachine stateMachine = new AdventureStateMachine();
+
+            Assert.That(stateMachine, Is.Not.Null);
+        }
+
+        [Test]
+        public void AStateMachineShouldTakeATextInput()
+        {
+            AdventureStateMachine stateMachine = new AdventureStateMachine();
+
+            stateMachine.Input(It.IsAny<string>());
+        }
+
+        [Test]
+        public void AStateMachineProduceATextStateThatIsNotEmptyOrNull()
+        {
+            AdventureStateMachine stateMachine = new AdventureStateMachine();
+
+            Assert.That(!String.IsNullOrEmpty(stateMachine.GetState()));
+        }
+
+    }
+}
